8b28f66b4dda0bc355eff0add5cf7777adff6bbc
Nikos D <nikossoftwaredev@gmail.com>
2024-06-14 02:08:37 +0300

    added meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,21 @@ import "./globals.css";
 import { TOKEN_ADDRESS, TOKEN_NAME } from "@/data/config";
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://trump-wif-birthday.fun"),
   title: TOKEN_NAME,
   description: `${TOKEN_NAME}  ca: ${TOKEN_ADDRESS}`,
+  applicationName: TOKEN_NAME,
+  robots: "index, follow",
+  openGraph: {
+    title: TOKEN_NAME,
+    description: `${TOKEN_NAME}  ca: ${TOKEN_ADDRESS}`,
+    images: "https://trump-wif-birthday.fun/images/logo.webp",
+  },
+  twitter: {
+    title: TOKEN_NAME,
+    description: `${TOKEN_NAME}  ca: ${TOKEN_ADDRESS}`,
+    images: "https://trump-wif-birthday.fun/images/logo.webp",
+  },
 };
 
 export default function RootLayout({
